88ac74bbd57cbeadbabeba981086936bb1032317
mariannebourcier <marianne.bourcier@gmail.com>
2022-05-16 13:40:01 -0600

    Implement char counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-  // --- our code goes here ---
+  $("#tweet-text").keyup(function() {
+    const typedText = $(this).val();
+    const numChars = 140 - typedText.length;
+    $("#charCounter").text(numChars);
+    if (numChars <= 0) {
+      $("#charCounter").css("color", "red");
+    } else {
+      $("#charCounter").css("color", "#545149");
+    }
+  });
 });
