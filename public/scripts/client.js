551e95e22e95e4712b8f40d81ab653b8ff142fed
mariannebourcier <marianne.bourcier@gmail.com>
2022-05-18 14:00:08 -0600

    Move tweet fetch to client.js file; Add loadTweet function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,36 +1,24 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-//connect to document
+//document ready
 $(document).ready(() => {
-  //render tweets in string
-  const renderTweets = (tweets) => {
-    for (let tweet of tweets) {
-      const $tweet = createTweetElement(tweet);
-      $("#tweets-container").prepend($tweet);
-    }
-  };
-  //fetch tweets data
-  const createTweetElement = (tweetData) => {
+  //create tweet
+   //fetch tweets data
+   const createTweetElement = (tweetData) => {
     const user = tweetData.user;
     const content = tweetData.content;
     const time = tweetData.created_at;
 
     const $tweetElement = `
-    <article class='tweet-container'>
+    <article id='tweet-container'>
     <header>
     <div>
-      <i class="fa-solid fa-kiwi-bird"></i> 
+      <img src="${user.avatars}"> 
       <span>${user.name}</span>
     </div>
     <p class="user-id">${user.handle}</p>
   </header>
   <p class="tweet-text-log">${content.text}</p>
   <footer>
-    <time>${tweetTime.format(time)}</time>
+    <span>${timeago.format(time)}</span>
     <div class="interaction-icons-container">
       <a href="#"><i class="flag-icon fa-solid fa-flag"></i></a>
       <a href="#"><i class="retweet-icon fa-solid fa-retweet"></i></i></a>
@@ -39,39 +27,42 @@ $(document).ready(() => {
   </footer>
 </article>`;
     return $tweetElement;
-
+  };
+  
+   //render tweets in string
+   const renderTweets = (tweets) => {
+    for (let tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $("#tweet-container").prepend($tweet);
+    }
   };
 
+   //get tweets
+   const loadTweets = () => {
+    $.getJSON("/tweets").done(function (tweets) {
+      $("#tweet-container").empty();
+      renderTweets(tweets);
+    });
+  };
+
+  //new tweet
+  //event listener
+  $( "form" ).submit('click', ( event ) => {
+    //prevent default
+    event.preventDefault();
+    //serialize
+    const tweet = $(this).serialize();
+
+    if (tweet.length > 141) return alert("Too many characters. Keep it at 140.");
+    if (tweet.length < 2) return alert("Tell us what's on your mind. Nobody's judging.");
+
+    $.post("/tweets", tweet).success(() => {
+      loadTweets();
+    });
+  });
+ 
 });
 
-
-
-
-//tweets
-  [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1651956573576
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1652042973576
-    }
-  ]
 
 // Test / driver code (temporary). Eventually will get this from the server.
 const tweetData = {
